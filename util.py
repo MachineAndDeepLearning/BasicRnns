e165216689537ac26fe301a780e7a51eff445de1
Krzysztof Jordan <krzysztof.jordan0@gmail.com>
2017-03-24 13:37:31 -0400

    Added unsupervised frost RNN

--- util.py
+++ util.py
@@ -45,3 +45,23 @@ def all_parity_pairs_with_sequence_labels(nbit):
 def remove_punctuation(s):
 	translator = str.maketrans('', '', string.punctuation)
 	return s.translate(translator)
+
+
+def get_robert_frost():
+	word2idx = {'Start': 0, 'END': 1}
+	current_idx = 2
+	sentences = []
+	for line in open('Data/robert_frost.txt', encoding='utf-8'):
+		line = line.strip()
+		if line:
+			tokens = remove_punctuation(line.lower()).split()
+			sentence = []
+			for t in tokens:
+				if t not in word2idx:
+					word2idx[t] = current_idx
+					current_idx += 1
+				idx = word2idx[t]
+				sentence.append(idx)
+
+			sentences.append(sentence)
+	return sentences, word2idx
